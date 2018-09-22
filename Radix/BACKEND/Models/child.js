e825224d778c0efdc08cad25e78d91d3557bae0a
oigresagetro <31581117+oigresagetro@users.noreply.github.com>
2018-09-22 17:32:30 -0600

    Cambios en modelos

--- Radix/BACKEND/Models/child.js
+++ Radix/BACKEND/Models/child.js
@@ -2,17 +2,18 @@ const mongoose = require('mongoose');
 
 const { Schema } = mongoose;
 
-const childSchema = new Schema({
-    fullName: {type : String, required : true},
-    ID      : {type : String,default   : 0   },
-    age     : {type : String, required : true},
-    gender  : {type : String, required : true},
+const medicine = new mongoose.Schema({
 
-    medicine: {
         medicineName        : {type : String, required : true},
         date                : {type : Date, required : true}
-    },
+        //creo que falta duracion.
+});
 
+const childSchema = new Schema({
+    fullName: {type : String, required : true},
+    id      : {type : Number, default   : 0  },
+    age     : {type : Number, required : true},
+    gender  : {type : String, required : true},
     treatments  : {type : String, required : true},
     background  : {
         medicine :   [medicine],
@@ -20,9 +21,6 @@ const childSchema = new Schema({
         diseases : [String]
     },
     ethnic  :{type: String,required : true }
-
-
-
 });
 
 module.exports = mongoose.model('Employee', childSchema);
