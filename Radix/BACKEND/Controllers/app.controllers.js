f96ec5e54805eff0161703a1c55bc0aff2b5aa62
oigresagetro <31581117+oigresagetro@users.noreply.github.com>
2018-09-22 16:31:22 -0600

    Update and rename employee.controllers.js to app.controllers.js

---
+++ Radix/BACKEND/Controllers/app.controllers.js
@@ -0,0 +1,59 @@
+const Parent = require('../Models/employee');
+//const Child = require( '../Models/child' );
+const appController = {};
+
+//Sec functions
+ appController.login = async(req, res) => {
+
+ };
+
+ appController.home = async(req, res) => {
+
+ }
+
+ appController.register = async(req, res) => {
+
+ }
+
+
+//Parent controllers
+ appController.getParent = async(req, res) => {
+     const { id } = req.params;
+     const parent = await Parent.findById(id);
+     res.json(parent);
+ }
+
+ appController.createParent = async(req, res) => {
+     const parent = new Parent({
+      email: req.body.email,
+      password: req.body.password,
+      number: req.body.number,
+      name: req.body.name,
+      id: req.body.id,
+      relationship: req.body.relationship
+  });
+  await parent.save();
+  res.json({status: 'Parent created'});
+ }
+
+//Child controllers
+ appController.getChild = async(req, res) => {
+
+ }
+
+appController.createChild = async(req, res) => {
+    const child = new Child({
+     name: req.body.name,
+     id: req.body.id,
+     age: req.body.age,
+     gender: req.body.gender,
+     background: req.body.background,
+     etnhic: req.body.etnhic
+ });
+ await child.save();
+ res.json({status: 'Child created'});
+}
+
+
+
+module.exports = appController;
